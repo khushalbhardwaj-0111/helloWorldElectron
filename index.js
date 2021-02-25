cb31b98b79ef95b3766f4e9a8f1c1b26da651c76
Khushal Bhardwaj <khushalbhardwaj0111>
2021-02-25 12:48:53 +0530

    more typos[spell]

--- index.js
+++ index.js
@@ -2,32 +2,32 @@
 // BrowserWindow to create a new BrowserWindow.
 const { app, BrowserWindow } = require("electron");
 
-// nodeIntegrations prefernces allow the app to acess all the node API.
+// nodeIntegrations preferences allow the app to access all the node API.
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-  win.loadFile("public/index.html");
+	win.loadFile("public/index.html");
 }
 
-// create a new BrowserWindow when electron app is initialzed.
+// create a new BrowserWindow when electron app is initialized.
 app.whenReady().then(createWindow);
 
 // quits the app if there are no longer any windows.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 // activate window when the app has no window activated
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
